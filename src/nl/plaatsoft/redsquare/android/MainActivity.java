6894e2b41afc8b66f9a9597ff240d59c32aa7432
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-03-26 14:31:56 +0100

    Add local high score storage

--- src/nl/plaatsoft/redsquare/android/MainActivity.java
+++ src/nl/plaatsoft/redsquare/android/MainActivity.java
@@ -5,12 +5,18 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import java.util.Comparator;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
     private GamePage gamePage;
@@ -61,6 +67,21 @@ public class MainActivity extends Activity {
 
         gamePage.setOnEventListener(new GamePage.OnEventListener() {
             public void onGameover(int score, int seconds, int level) {
+                try {
+                    JSONArray scoresJSON = new JSONArray(preferences.getString("scores", "[]"));
+
+                    JSONObject newScoreJSON = new JSONObject();
+                    newScoreJSON.put("name", preferences.getString("name", "Anonymous"));
+                    newScoreJSON.put("score", score);
+                    scoresJSON.put(newScoreJSON);
+
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("scores", scoresJSON.toString());
+                    editor.apply();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+
                 gameoverScoreLabel.setText(String.format(scoreLabelString, score));
 
                 gameoverTimeLabel.setText(String.format(timeLabelString, seconds / 60, seconds % 60));
@@ -80,8 +101,33 @@ public class MainActivity extends Activity {
         });
 
         // High Score page
+        ScoreAdapter scoresAdapter = new ScoreAdapter(this);
+        ListView localScoresList = (ListView)findViewById(R.id.local_scores_list);
+        localScoresList.setAdapter(scoresAdapter);
+
         ((Button)findViewById(R.id.menu_highscore_button)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                try {
+                    scoresAdapter.clear();
+                    JSONArray scoresJSON = new JSONArray(preferences.getString("scores", "[]"));
+                    for (int i = 0; i < scoresJSON.length(); i++) {
+                        JSONObject scoreJSON = scoresJSON.getJSONObject(i);
+                        scoresAdapter.add(new Score(
+                            scoreJSON.getString("name"),
+                            scoreJSON.getInt("score")
+                        ));
+                    }
+
+                    scoresAdapter.sort(new Comparator<Score>() {
+                        public int compare(Score a, Score b) {
+                            return b.getScore() - a.getScore();
+                        }
+                    });
+                    scoresAdapter.notifyDataSetChanged();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+
                 menuPage.setVisibility(View.GONE);
                 highscorePage.setVisibility(View.VISIBLE);
             }
@@ -111,6 +157,14 @@ public class MainActivity extends Activity {
 
         // Settings page
         EditText settingsNameInput = (EditText)findViewById(R.id.settings_name_input);
+        settingsNameInput.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    hideSystemUI();
+                }
+                return false;
+            }
+        });
 
         ((Button)findViewById(R.id.menu_settings_button)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -137,15 +191,19 @@ public class MainActivity extends Activity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-            );
+            hideSystemUI();
         }
+    }
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
     }
 
     public void onResume() {
---
+++ src/nl/plaatsoft/redsquare/android/Score.java
@@ -0,0 +1,19 @@
+package nl.plaatsoft.redsquare.android;
+
+public class Score {
+    private String name;
+    private int score;
+
+    public Score(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
