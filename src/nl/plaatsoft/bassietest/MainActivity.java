d55954c6e058131ef72982cf49a0f26f9b4502d0
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-11-06 20:31:42 +0100

    Fixed some indentation errors

--- src/nl/plaatsoft/bassietest/MainActivity.java
+++ src/nl/plaatsoft/bassietest/MainActivity.java
@@ -1,6 +1,5 @@
 package nl.plaatsoft.bassietest;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
--- src/nl/plaatsoft/bassietest/SettingsActivity.java
+++ src/nl/plaatsoft/bassietest/SettingsActivity.java
@@ -1,6 +1,5 @@
 package nl.plaatsoft.bassietest;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
