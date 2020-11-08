565675e3e38403993214c6d5443a5cbfc05d440a
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2020-11-08 13:06:21 +0100

    Remove config file and add sample config

--- src/nl/plaatsoft/redsquare/android/Config.java
+++
@@ -1,8 +0,0 @@
-package nl.plaatsoft.redsquare.android;
-
-public class Config {
-    private Config() {}
-
-    public static final String API_URL = "https://bastiaan.plaatsoft.nl/redsquare-android-score.php";
-    public static final String API_KEY = "3eb9fc53b53a589e182605c95334e227";
-}
