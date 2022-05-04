26e496f183b678e65ab0f91d9ee5d1832cbfcd57
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2022-05-04 11:56:21 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,35 +1,77 @@
-# keytool -genkey -validity 10000 -keystore key.keystore -keyalg RSA -keysize 2048 -storepass nos-android -keypass nos-android
+#!/bin/bash
 
-PATH=$PATH:~/android-sdk/build-tools/30.0.2:~/android-sdk/platform-tools
-PLATFORM=C:\\Users\\bplaat\\android-sdk\\platforms\\android-30\\android.jar
+# --- Bassie Android Build Script v1.1 (modified) ---
 
-if [ "$1" == "log" ]; then
+# The default gradle Android build toolchain is so slow and produces bloated apks
+# So I use this nice build shell script to get the job done!
+
+# Install the OpenJDK JDK 8 and add all binaries to your path
+# Install your Android SDK and set the $ANDROID_HOME with the path with
+# the following packages: platform-tools platforms;android-31 build-tools;31.0.0
+# Run this script with bash on Linux, macOS or a Git Bash / Msys install on Windows
+# For inspecting apks you need to install Jadx GUI and add it to your path
+
+PATH=$PATH:$ANDROID_HOME/build-tools/31.0.0:$ANDROID_HOME/platform-tools
+PLATFORM=$ANDROID_HOME/platforms/android-31/android.jar
+
+name="nos"
+package="nl.plaatsoft.nos.android"
+password="nos-android"
+main_activity=".MainActivity"
+
+if [ "$1" == "key" ]; then
+    keytool -genkey -validity 7120 -keystore keystore.jks -keyalg RSA -keysize 4096 -storepass $password -keypass $password
+
+elif [ "$1" == "log" ]; then
     adb logcat -c
     adb logcat *:E
+
+elif [ "$1" == "clear" ]; then
+    echo "Clearing data and opening application"
+    adb shell pm clear $package
+    adb shell am start -n $package/$main_activity
+
 else
-    mkdir resources
-    if
-        aapt2 compile --dir res -o resources &&
-        aapt2 link $(find resources -name *.flat) --manifest AndroidManifest.xml --java src -I $PLATFORM -o nos-unaligned.apk
-    then
-        if javac -Xlint -cp "$PLATFORM;classes" -d classes $(find src -name *.java); then
+    mkdir res-compiled
+    echo "Compiling resources files"
+    if aapt2 compile --dir res -o res-compiled; then
+        if aapt2 link res-compiled/*.flat --manifest AndroidManifest.xml --java src -I $PLATFORM -o $name-unaligned.apk; then
 
-            find classes -name *.class > classes.txt
-            d8.bat --release --lib $PLATFORM @classes.txt
-            aapt add nos-unaligned.apk classes.dex
+            echo "Compiling java code"
+            find src -name *.java > sources.txt
+            if javac -Xlint -cp "$PLATFORM:src-compiled" -d src-compiled @sources.txt; then
 
-            zipalign -f -p 4 nos-unaligned.apk nos.apk
+                echo "Packing and signing application"
+                find src-compiled -name *.class > classes.txt
+                if [ "$(uname -s)" == "Linux" ] || [ "$(uname -s)" == "Darwin" ]; then
+                    d8 --release --lib $PLATFORM --min-api 21 @classes.txt
+                else
+                    d8.bat --release --lib $PLATFORM --min-api 21 @classes.txt
+                fi
+                aapt add $name-unaligned.apk classes.dex > /dev/null
 
-            apksigner.bat sign --ks key.keystore --ks-pass pass:nos-android --ks-pass pass:nos-android nos.apk
+                zipalign -f -p 4 $name-unaligned.apk $name.apk
 
-            adb install -r nos.apk
-            adb shell am start -n nl.plaatsoft.nos.android/.MainActivity
+                if [ "$(uname -s)" == "Linux" ] || [ "$(uname -s)" == "Darwin" ]; then
+                    apksigner sign --v4-signing-enabled false --ks keystore.jks --ks-pass pass:$password --ks-pass pass:$password $name.apk
+                else
+                    apksigner.bat sign --v4-signing-enabled false --ks keystore.jks --ks-pass pass:$password --ks-pass pass:$password $name.apk
+                fi
 
-            rm -r resources nos-unaligned.apk src/nl/plaatsoft/nos/android/R.java classes/nl classes.dex classes.txt
-        else
-            rm -r resources nos-unaligned.apk src/nl/plaatsoft/nos/android/R.java classes/nl
+                if [ "$1" == "inspect" ]; then
+                    echo "Inspecting application"
+                    jadx-gui $name.apk
+                else
+                    echo "Installing and opening application"
+                    adb install -r $name.apk
+                    adb shell am start -n $package/$main_activity
+                fi
+
+                rm -f classes.txt classes.dex
+            fi
+            rm -f -r src-compiled/nl sources.txt
         fi
-    else
-        rm -r resources nos-unaligned.apk
+        rm -f -r $name-unaligned.apk src/${package//\./\/}/R.java
     fi
+    rm -f -r res-compiled
 fi
