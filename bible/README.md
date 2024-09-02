a5a402b470bd7a15e445c44115fed027df530d5b
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-09-02 08:00:47 +0200

    Bible: Add NBV 2004 translation

--- bible/README.md
+++ bible/README.md
@@ -9,6 +9,7 @@ Bible translations are scraped from [de-bijbel.nl](https://de-bijbel.nl/) and [d
 - [NBV21 (NBV21)](https://www.debijbel.nl/bijbel/NBV21/GEN.1)
 - [Bijbel in Gewone Taal (BGT)](https://www.debijbel.nl/bijbel/BGT/GEN.1)
 - [Herziene Statenvertaling (HSV)](https://www.debijbel.nl/bijbel/HSV/GEN.1)
+- [Nieuwe Bijbelvertaling](https://www.debijbel.nl/bijbel/NBV/GEN.1)
 
 ### English
 - [New International Version (NIV)](https://www.die-bibel.de/bibel/NIV/GEN.1)
--- bible/build.sh
+++ bible/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 if [ "$1" = "downloader" ]; then
     cd downloader; cargo build --release; cd ..
+    # Dutch bible translations
     ./downloader/target/release/bible-dl nbv21 -o assets/bibles/nbv21.bible
     ./downloader/target/release/bible-dl bgt -o assets/bibles/bgt.bible
+    ./downloader/target/release/bible-dl nbv -o assets/bibles/nbv.bible
     ./downloader/target/release/bible-dl hsv -o assets/bibles/hsv.bible
+    # English bible translations
     ./downloader/target/release/bible-dl niv -o assets/bibles/niv.bible
     ./downloader/target/release/bible-dl kjv -o assets/bibles/kjv.bible
     exit
