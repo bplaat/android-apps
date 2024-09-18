c4eed5390b0cc07e2624b77a2723cd6576134dbf
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-09-18 22:23:39 +0200

    Bible: Move bible-dl tool

--- bible/build.sh
+++ bible/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
-if [ "$1" = "downloader" ]; then
-    cd downloader; cargo build --release; cd ..
+if [ "$1" = "tools" ]; then
+    cd tools/bible-dl; cargo build --release; cd ../..
     # Dutch bible translations
-    ./downloader/target/release/bible-dl nbv21 -o assets/bibles/nbv21.bible
-    ./downloader/target/release/bible-dl bgt -o assets/bibles/bgt.bible
-    ./downloader/target/release/bible-dl nbv -o assets/bibles/nbv.bible
-    ./downloader/target/release/bible-dl hsv -o assets/bibles/hsv.bible
+    ./tools/bible-dl/target/release/bible-dl nbv21 -o assets/bibles/nbv21.bible
+    ./tools/bible-dl/target/release/bible-dl bgt -o assets/bibles/bgt.bible
+    ./tools/bible-dl/target/release/bible-dl nbv -o assets/bibles/nbv.bible
+    ./tools/bible-dl/target/release/bible-dl hsv -o assets/bibles/hsv.bible
     # English bible translations
+    ./tools/bible-dl/target/release/bible-dl niv -o assets/bibles/niv.bible
+    ./tools/bible-dl/target/release/bible-dl kjv -o assets/bibles/kjv.bible
     ./downloader/target/release/bible-dl niv -o assets/bibles/niv.bible
     ./downloader/target/release/bible-dl kjv -o assets/bibles/kjv.bible
     exit
