6156fdba943c372cd3bb80f87b3b110cedb6386c
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-09-04 21:51:43 +0200

    Bible: Update scrape.sh paths

--- bin/bible/tools/scrape.sh
+++ bin/bible/tools/scrape.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
-cargo build
 
 for translation in nbv21 bgt nbv hsv niv kjv lu17 eue; do
-    ./tools/target/debug/bible-dl $translation -o assets/bibles/$translation.bible
+    cargo run --bin bible-dl -- $translation -o ../assets/bibles/$translation.bible
 done
 
-./tools/target/debug/convert-ops --name Hemelhoog --abbreviation hh \
+cargo run --bin convert-ops -- --name Hemelhoog --abbreviation hh \
     --language nl --copyright "Copyright © 2015 Uitgeverij Boekencentrum" \
-    --songs tools/tmp/hh_songs.json --lyrics tools/tmp/hh_lyrics.json \
-    -o assets/songbundles/hh.songbundle
+    --songs tmp/hh_songs.json --lyrics tmp/hh_lyrics.json \
+    -o ../assets/songbundles/hh.songbundle
 
-./tools/target/debug/scrape-opwekking -o assets/songbundles/opw.songbundle
+cargo run --bin scrape-opwekking -- -o ../assets/songbundles/opw.songbundle
